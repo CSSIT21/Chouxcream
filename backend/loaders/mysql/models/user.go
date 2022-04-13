195a042cf166c03556eb31719ef7094b27af4e3d
Thun (Mac Swift 3) <bsthun@gmail.com>
2022-04-13 17:22:11 +0700

    Edit gorm model

--- backend/loaders/mysql/models/user.go
+++ backend/loaders/mysql/models/user.go
@@ -2,24 +2,14 @@ package models
 
 import (
 	"time"
-
-	"backend/types/enum"
 )
 
 type User struct {
-	Id               *uint64        `gorm:"primaryKey"`
-	Firstname        *string        `gorm:"type:VARCHAR(255); not null"`
-	Lastname         *string        `gorm:"type:VARCHAR(255); not null"`
-	Email            *string        `gorm:"type:VARCHAR(255); index:email,unique; not null"`
-	EmailVerified    *bool          `gorm:"default:false; not null"`
-	AvatarUrl        *string        `gorm:"type:TEXT"` // Null represents no avatar picture
-	Password         *string        `gorm:"type:VARCHAR(255); not null"`
-	Type             *enum.UserType `gorm:"type:ENUM('student', 'normal_mod', 'org_mod', 'sale', 'admin'); not null"`
-	SelfOrganization *string        `gorm:"type:VARCHAR(255); null"`
-	SelfFaculty      *string        `gorm:"type:VARCHAR(255); null"`
-	RefererId        *uint64        `gorm:"null"`
-	Referer          *User          `gorm:"foreignKey:RefererId"`
-	Suspension       *string        `gorm:"type:VARCHAR(255)"` // Null represents account still active
-	CreatedAt        *time.Time     `gorm:"not null"`          // Embedded field
-	UpdatedAt        *time.Time     `gorm:"not null"`          // Embedded field
+	Id            *uint64    `gorm:"primaryKey"`
+	Firstname     *string    `gorm:"type:VARCHAR(255); not null"`
+	Lastname      *string    `gorm:"type:VARCHAR(255); not null"`
+	Email         *string    `gorm:"type:VARCHAR(255); index:email,unique; not null"`
+	EmailVerified *bool      `gorm:"default:false; not null"`
+	CreatedAt     *time.Time `gorm:"not null"` // Embedded field
+	UpdatedAt     *time.Time `gorm:"not null"` // Embedded field
 }
